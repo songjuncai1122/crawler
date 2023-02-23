f9b80b6798e7f2d52330aaaa9ad724d24de12006
songjuncai <czsongjuncai@163.com>
2023-02-23 10:31:43 +0800

    xpath

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/songjuncai1122/crawler
 
 go 1.19
+
+require (
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,34 +1,66 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"io"
+	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-
-	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Printf("read content failed:%v", err)
 		return
 	}
+	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	if err != nil {
+		fmt.Printf("xxxx %v", err)
+	}
+	nodes := htmlquery.Find(doc, `//div[@class="news_li"]/h2/a[@target="_blank"]`)
 
-	fmt.Println("body:", string(body))
+	for _, node := range nodes {
+		fmt.Printf("fetch card", node.FirstChild.Data)
+	}
+}
+
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
