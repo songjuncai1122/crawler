7a03595299ed3f4ae51d62d1c1d9c6db4c23db16
songjuncai <czsongjuncai@163.com>
2023-07-12 11:18:03 +0800

    tag v0.1.2增加日志功能

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.19
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,42 +1,37 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"github.com/songjuncai1122/crawler/collect"
+	"github.com/songjuncai1122/crawler/log"
 	"github.com/songjuncai1122/crawler/proxy"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"time"
 )
 
 func main() {
+	plugin, c := log.NewFilePlugin("./log.txt", zapcore.InfoLevel)
+	defer c.Close()
+	logger := log.NewLogger(plugin)
+	logger.Info("log init end")
+
 	proxyURLs := []string{"http://127.0.0.1:7890", "http://127.0.0.1:7890"}
 	p, err := proxy.RoundRobinProxySwithcher(proxyURLs...)
 	if err != nil {
-		fmt.Println("RoundRobin failed")
+		logger.Error("Roundboinproxy failed")
 	}
 
 	url := "https://google.com"
 	var f collect.Fetcher = collect.BrowserFetch{
-		Timeout: 30000 * time.Millisecond,
+		Timeout: 3000 * time.Millisecond,
 		Proxy:   p,
 	}
 	body, err := f.Get(url)
 	if err != nil {
-		fmt.Println("read content failed:%v", err)
+		logger.Error("read content filed",
+			zap.Error(err),
+		)
 		return
 	}
-	fmt.Println(string(body))
-
-	// 加载HTML文档
-	doc, err := goquery.NewDocumentFromReader((bytes.NewReader(body)))
-	if err != nil {
-		fmt.Println("read content ffailed:%v", err)
-	}
-
-	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
-		// 获取匹配元素的文本
-		title := s.Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
+	logger.Info("get content", zap.Int("len", len(body)))
 }
