be3999703872f0770061c31bb20ee7aa78c82e5b
songjuncai <czsongjuncai@163.com>
2023-07-12 10:14:22 +0800

    tag v0.0.9封装请求方法

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/songjuncai1122/crawler
 go 1.19
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,61 +1,34 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/songjuncai1122/crawler/collect"
+	"time"
 )
 
-var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
-
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%d", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterminEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return ioutil.ReadAll(utf8Reader)
-
-}
-
-func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
-	bytes, err := r.Peek(1024)
-
-	if err != nil {
-		fmt.Printf("fetch error:%v", err)
-		return unicode.UTF8
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
-}
-
 func main() {
-	//url := "https://www.chinanews.com.cn"
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
-
+	url := "https://book.douban.com/subject/1007305/"
+	var f collect.Fetcher = collect.BrowserFetch{
+		Timeout: 3000 * time.Millisecond,
+	}
+	body, err := f.Get(url)
 	if err != nil {
-		fmt.Printf("read content failed :%v", err)
+		fmt.Println("read content failed:%v", err)
 		return
 	}
-	matches := headerRe.FindAllSubmatch(body, -1)
-	for _, m := range matches {
-		fmt.Println("fetch card news:", string(m[1]))
+	//fmt.Println(string(body))
+
+	// 加载HTML文档
+	doc, err := goquery.NewDocumentFromReader((bytes.NewReader(body)))
+	if err != nil {
+		fmt.Println("read content ffailed:%v", err)
 	}
 
+	doc.Find("div.small_cardcontent__BTALp h2").Each(func(i int, s *goquery.Selection) {
+		// 获取匹配元素的文本
+		title := s.Text()
+		fmt.Printf("Review %d: %s\n", i, title)
+	})
 }
